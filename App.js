a0d16ef944840b4c567aa326a9d9e9e9cc456a9d
Priyansh <PriyanshYadav1012@gmail.com>
2024-01-31 01:30:47 +0530

    Expo Create App

---
+++ App.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className=" flex flex-1 items-center bg-slate-800 justify-center" >
+      <Text className="text-white">Hello World</Text>
+      
+    </View>
+  );
+}
+
+
